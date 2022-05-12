230a49d6fc11760300b8969969a3e5d653e4fbc4
Emma Vestesson <emma.vestesson@gmail.com>
2022-05-12 13:18:14 +0100

    Make points bigger!

--- 1_exploratory_plots.R
+++ 1_exploratory_plots.R
@@ -9,5 +9,5 @@ ggplot(economics, aes(date, unemploy)) + geom_line(colour = 'blue')
 # Second plot
 
 ggplot(economics_long, aes(date, value01, colour = variable)) +
-  geom_line() + geom_point(size =2)
+  geom_line() + geom_point(size = 5)
 
